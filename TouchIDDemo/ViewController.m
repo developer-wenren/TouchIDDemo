617ea5554846382133d2cf0bb233c6311a306834
wrcj <wrcj12138@163.com>
2015-04-18 17:40:46 +0800

    remove deriveData

--- TouchIDDemo/ViewController.m
+++ TouchIDDemo/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+@import LocalAuthentication;
+
+
 @interface ViewController ()
 
 @end
@@ -17,11 +20,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self addButtonStartTouchID];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)addButtonStartTouchID
+{
+    UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(100, 100, 200, 60)];
+    button.center = CGPointMake(160, 200);
+    [button setBackgroundColor:[UIColor grayColor]];
+    [button setTitle:@"touchID" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(touchID) forControlEvents:UIControlEventTouchDown];
+    
+    [self.view addSubview:button];
+    
+    
+}
+
+-(void)touchID
+{
+    LAContext *laContext = [[LAContext alloc]init];
+    
+    laContext.localizedFallbackTitle = @"";
+    
+    NSError *error2 = [[NSError alloc]init];
+    
+    if ([laContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error2])
+    {
+        [laContext evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:@"哈哈" reply:^(BOOL success, NSError *error) {
+            
+            if (!error)
+            {
+                NSLog(@"success");
+                
+            }else
+            {
+                NSLog(@"%@",error);
+                
+            }
+            
+        }];
+        
+        
+    }else
+    {
+        NSLog(@"%@",[error2 description]);
+    }
 }
 
 @end
