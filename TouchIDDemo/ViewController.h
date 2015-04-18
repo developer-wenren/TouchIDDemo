f07260b0a317d70be2201aa6789178443b282ff6
wrcj <wrcj12138@163.com>
2015-04-18 16:07:51 +0800

    Initial Commit

---
+++ TouchIDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchIDDemo
+//
+//  Created by zjsruxxxy3 on 15/4/18.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TouchIDDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TouchIDDemo
+//
+//  Created by zjsruxxxy3 on 15/4/18.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
